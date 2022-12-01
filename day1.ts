2aa126fd06f63fe402c7395a02b87e443134f20a
Chris Jewell <chrisj@hotdoc.com.au>
2022-12-01 16:38:00 +1100

    Day 1

---
+++ day1.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+
+const sum: (list: number[]) => number = (list) => list.reduce((acc, elem) => acc + elem);
+const readInput: (day: number) => string = (number) => fs.readFileSync(`./inputs/day${number}.txt`).toString();
+
+function stringToElfCaloriesTotal(str: string): number[] {
+  return str
+    .trim()
+    .split("\n\n")
+    .map((str) => str.split("\n"))
+    .map((list) => list.map((x) => parseInt(x)))
+    .map((elem) => sum(elem));
+}
+
+function mostCalories(str: string): number {
+  return Math.max(...stringToElfCaloriesTotal(str));
+}
+
+function threeMostCalories(str: string): number[] {
+  return stringToElfCaloriesTotal(str)
+    .sort((a, b) => b - a)
+    .slice(0, 3);
+}
+
+const input = readInput(1);
+
+console.log(mostCalories(input.toString()));
+console.log(sum(threeMostCalories(input.toString())));
